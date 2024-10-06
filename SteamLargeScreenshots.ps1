b5d0e311dfe56dca2209b98bcc027ebea9a72be9
mattiejas <mattiejastje@gmail.com>
2024-10-06 13:21:10 +0100

    Set quality for large image export.

--- SteamLargeScreenshots.ps1
+++ SteamLargeScreenshots.ps1
@@ -13,7 +13,7 @@ ForEach ($resolution In 26210175..26210225) {
     If ($rem -eq 0) {
       Write-Host "$num -- $width x $height = $resolution"
       $image = New-Object System.Drawing.Bitmap $width,$height
-      $filename = "d:\{0}_{1}.jpg" -f $datestr,$num
+      $filename = "{0}_{1}.jpg" -f $datestr,$num
       Write-Host "  $filename"
       [Int32]$fontsize = [Math]::Min($width, $height) / 2
       $font = new-object System.Drawing.Font "Ariel",$fontsize
@@ -22,8 +22,12 @@ ForEach ($resolution In 26210175..26210225) {
       $graphics = [System.Drawing.Graphics]::FromImage($image) 
       $graphics.FillRectangle($brushBg,0,0,$image.Width,$image.Height) 
       $graphics.DrawString("$num",$font,$brushFg,($image.Width - 2 * $fontsize) / 2,($image.Height - 2 * $fontsize) / 2) 
-      $graphics.Dispose() 
-      $image.Save($filename, [System.Drawing.Imaging.ImageFormat]::Jpeg)
+      $graphics.Dispose()
+      $jpegcodec = [System.Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | Where-Object { $_.MimeType -eq "image/jpeg" }
+      $encparams = New-Object System.Drawing.Imaging.EncoderParameters(1)
+      $encquality = New-Object System.Drawing.Imaging.EncoderParameter([System.Drawing.Imaging.Encoder]::Quality, 90)
+      $encparams.Param[0] = $encquality
+      $image.Save($filename, $jpegcodec, $encparams)
       $num += 1
       Break
     }
